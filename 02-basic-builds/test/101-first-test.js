45235c82051ec7dd5000cdfbd208f174e66cf57d
Dylan Hassinger <dylanhassinger@gmail.com>
2016-02-18 17:57:27 -0600

    nc

---
+++ 02-basic-builds/test/101-first-test.js
@@ -0,0 +1,15 @@
+// setup
+
+	var test = require("unit.js");
+	
+// tests 
+
+	describe('This is a suite', function() {
+	
+		it('This is a test case', function() {
+		
+			test.assert(true === true);
+	
+		});
+	    
+	});
